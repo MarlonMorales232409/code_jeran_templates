bc2c01145f5a0da4795fe7b1d4a9a9ddab611c7d
MarlonMorales232409 <marlon.morales2324@icloud.com>
2022-10-25 17:12:40 +0200

    creating coffe landing

---
+++ coffe_landing/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <Typography variant='h1' color="primary">Landing Page Base Template</Typography>
+  )
+}
+
+export default Home
